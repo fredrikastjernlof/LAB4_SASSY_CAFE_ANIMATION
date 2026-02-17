acf8b16b132641ea7a2eeea661f88bfd95163671
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-02-17 18:55:55 +0100

    Lägger grunden för animation.html, uppdaterar vite.config.js och lägger till animering i huvudmenyerna

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                SASS: resolve(__dirname, "sass.html")
+                SASS: resolve(__dirname, "sass.html"),
+                Animation: resolve(__dirname, "animation.html")
             },
         },
     },
